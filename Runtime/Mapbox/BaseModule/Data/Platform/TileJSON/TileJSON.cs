a8b704e82603b3f1f4dd4138d575cda3988f4526
Baran Kahyaoglu <baran.kahyaoglu@mapbox.com>
2025-03-03 18:53:28 +0300

    add a null check to tileJson for missing tilejson data

--- Runtime/Mapbox/BaseModule/Data/Platform/TileJSON/TileJSON.cs
+++ Runtime/Mapbox/BaseModule/Data/Platform/TileJSON/TileJSON.cs
@@ -5,49 +5,54 @@ using Newtonsoft.Json;
 
 namespace Mapbox.BaseModule.Data.Platform.TileJSON
 {
-	public class TileJSON
-	{
+    public class TileJSON
+    {
 
-		private IFileSource _fileSource;
-		private int _timeout;
-
-
-		public IFileSource FileSource { get { return _fileSource; } }
+        private IFileSource _fileSource;
+        private int _timeout;
 
 
-		public TileJSON(IFileSource fileSource, int timeout)
-		{
-			_fileSource = fileSource;
-			_timeout = timeout;
-		}
+        public IFileSource FileSource { get { return _fileSource; } }
 
 
-		public IAsyncRequest Get(string tilesetName, Action<TileJSONResponse> callback)
-		{
-			string url = string.Format(
-				"{0}v4/{1}.json?secure"
-				, Constants.Map.BaseAPI
-				, tilesetName
-			);
-
-			return _fileSource.Request(
-				url
-				, (Response response) =>
-				{
-					string json = Encoding.UTF8.GetString(response.Data);
-					TileJSONResponse tileJSONResponse = JsonConvert.DeserializeObject<TileJSONResponse>(json);
-					if (tileJSONResponse != null)
-					{
-						tileJSONResponse.Source = tilesetName;
-					}
-					callback(tileJSONResponse);
-				}
-				, _timeout
-			);
-		}
+        public TileJSON(IFileSource fileSource, int timeout)
+        {
+            _fileSource = fileSource;
+            _timeout = timeout;
+        }
 
 
+        public IAsyncRequest Get(string tilesetName, Action<TileJSONResponse> callback)
+        {
+            string url = string.Format(
+                "{0}v4/{1}.json?secure"
+                , Constants.Map.BaseAPI
+                , tilesetName
+            );
+
+            return _fileSource.Request(
+                url
+                , (Response response) =>
+                {
+                    if (response != null && response.Data != null && response.Data.Length > 0)
+                    {
+                        string json = Encoding.UTF8.GetString(response.Data);
+                        TileJSONResponse tileJSONResponse = JsonConvert.DeserializeObject<TileJSONResponse>(json);
+                        if (tileJSONResponse != null)
+                        {
+                            tileJSONResponse.Source = tilesetName;
+                        }
+                        callback(tileJSONResponse);
+                    }
+
+                    callback(null);
+                }
+                , _timeout
+            );
+        }
 
 
-	}
+
+
+    }
 }
